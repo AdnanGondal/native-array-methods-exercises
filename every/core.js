4a67d4458c4d9f6c30fde50615d7161777e6ec1c
Adnan Gondal <adnangondal1@hotmail.com>
2021-08-20 12:45:52 +0100

    every done

--- every/core.js
+++ every/core.js
@@ -1,35 +1,44 @@
 // Check to see if all elements in an array
 // are even numbers.
 
-function allEven (input) {
-  return input;
-};
+function allEven(input) {
+  return input.every((num) => num % 2 == 00);
+}
 
 // Check to see if all elements in an array
 // are of the same type.
 
-function allSameType (input) {
-  return input;
-};
+function allSameType(input) {
+  return input.every((elem) => typeof elem === typeof input[0]);
+}
 
 // Check to see if every element in the matrix is
 // an array and that every element in the array is
 // greater than 0.
 
-function positiveMatrix (input) {
-  return input;
-};
+function positiveMatrix(input) {
+  return input.every(
+    (elem) => Array.isArray(elem) && elem.every((num) => num > 0)
+  );
+}
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
-function allSameVowels (input) {
-  return input;
-};
+function countVowels(word) {
+  return [...new Set(word.split(""))].reduce((acc, letter) => {
+    if (/[aeiou]/.test(letter)) acc++;
+    return acc;
+  }, 0);
+}
+
+function allSameVowels(input) {
+  return input.every((word) => countVowels(word) <= 1);
+}
 
 module.exports = {
   allEven,
   allSameType,
   positiveMatrix,
-  allSameVowels
+  allSameVowels,
 };
