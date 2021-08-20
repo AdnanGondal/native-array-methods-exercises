9c97b7d98c02d8aa7897f694fa5ff686fbbcec7a
Adnan Gondal <adnangondal1@hotmail.com>
2021-08-20 14:59:44 +0100

    reduce done

--- reduce/core.js
+++ reduce/core.js
@@ -1,24 +1,30 @@
-function sum (array) {
-  // your code here
+function sum(array) {
+  return array.reduce((acc, val) => acc + val, 0);
 }
 
-function productAll (array) {
-  // your code here
+function productAll(array) {
+  return array.reduce(
+    (acc, row) => acc * row.reduce((prod, elem) => prod * elem),
+    1
+  );
 }
 
-function objectify (array) {
-  // your code here
-
+function objectify(array) {
+  return array.reduce((kvpair, elem) => {
+    kvpair[elem[0]] = elem[1];
+    return kvpair;
+  }, {});
 }
 
-function luckyNumbers (array) {
-  // your code here
+function luckyNumbers(array) {
+  return array.reduce((sent, num, i) => {
+    return i === array.length - 1 ? sent + "and " + num : sent + num + ", ";
+  }, "Your lucky numbers are: ");
 }
-
 
 module.exports = {
   sum: sum,
   productAll: productAll,
   objectify: objectify,
-  luckyNumbers: luckyNumbers
+  luckyNumbers: luckyNumbers,
 };
