23c44922bbc38a448c2ed2a9801c23cbe9db3927
Adnan Gondal <adnangondal1@hotmail.com>
2021-08-20 12:10:35 +0100

    filter done

--- filter/core.js
+++ filter/core.js
@@ -1,22 +1,29 @@
-function onlyEven (array) {
-  // your code here
-};
+function onlyEven(array) {
+  return array.filter((num) => num % 2 == 0);
+}
 
-function onlyOneWord (array) {
-  // your code here
-};
+function onlyOneWord(array) {
+  return array.filter((str) => !str.includes(" "));
+}
 
-function positiveRowsOnly (array) {
-  // your code here
-};
+function positiveRowsOnly(array) {
+  return array.filter((row) => row.every((num) => num >= 0));
+}
 
-function allSameVowels (array) {
-  // your code here
-};
+function countVowels(word) {
+  return [...new Set(word.split(""))].reduce((acc, letter) => {
+    if (/[aeiou]/.test(letter)) acc++;
+    return acc;
+  }, 0);
+}
+function allSameVowels(array) {
+  //return countVowels("amalagam");
+  return array.filter((word) => countVowels(word) <= 1);
+}
 
 module.exports = {
   onlyEven: onlyEven,
   onlyOneWord: onlyOneWord,
   positiveRowsOnly: positiveRowsOnly,
-  allSameVowels: allSameVowels
+  allSameVowels: allSameVowels,
 };
