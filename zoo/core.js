fd08d683bea34c6bfdd7738e2f753a9da18961ad
Adnan Gondal <adnangondal1@hotmail.com>
2021-09-05 10:21:36 +0100

    mostly done

--- zoo/core.js
+++ zoo/core.js
@@ -1,3 +1,4 @@
+const { animals } = require("./data");
 const zooData = require("./data");
 
 function entryCalculator(entrants) {
@@ -33,30 +34,126 @@ function schedule(dayName) {
   } else return readable;
 }
 
-function animalCount(species) {}
+function animalCount(species) {
+  const animalCount = zooData.animals.reduce((obj, animal) => {
+    obj[animal.name] = animal.residents.length;
+    return obj;
+  }, {});
+
+  if (species) return animalCount[species];
+  return animalCount;
+}
 
 function animalMap(options) {
-  // your code here
+  const animalLocn = { NE: [], NW: [], SE: [], SW: [] };
+  if (!options || !options.includeNames) {
+    zooData.animals.forEach((animal) => {
+      animalLocn[animal.location].push(animal.name);
+    });
+    return animalLocn;
+  }
+
+  if (options.includeNames && !options.sex) {
+    zooData.animals.forEach((animal) => {
+      animalLocn[animal.location].push(
+        animal.residents.reduce((obj, name) => {
+          if (!Array.isArray(obj[animal.name])) {
+            obj[animal.name] = [];
+          }
+          obj[animal.name].push(name.name);
+          return obj;
+        }, {})
+      );
+    });
+  } else if (options.includeNames && options.sex) {
+    zooData.animals.forEach((animal) => {
+      animalLocn[animal.location].push(
+        animal.residents.reduce((obj, name) => {
+          if (!Array.isArray(obj[animal.name])) {
+            obj[animal.name] = [];
+          }
+          if (name.sex === options.sex) obj[animal.name].push(name.name);
+          return obj;
+        }, {})
+      );
+    });
+  }
+  return animalLocn;
 }
 
 function animalPopularity(rating) {
   // your code here
+  const popObj = zooData.animals.reduce((obj, anim) => {
+    if (!Array.isArray(obj[anim.popularity.toString()]))
+      obj[anim.popularity.toString()] = [];
+    obj[anim.popularity.toString()].push(anim.name);
+    return obj;
+  }, {});
+  if (rating) {
+    return popObj[rating];
+  } else return popObj;
 }
 
 function animalsByIds(ids) {
-  // your code here
+  if (!ids) return [];
+  if (typeof ids === "string") {
+    return [animals.find((animal) => animal.id === ids)];
+  } else if (Array.isArray(ids)) {
+    return animals.filter((animal) => {
+      for (let id of ids) {
+        if (id === animal.id) return true;
+      }
+    });
+  }
 }
 
 function animalByName(animalName) {
-  // your code here
+  const anim = {};
+  if (animalName) {
+    zooData.animals.forEach((animal) => {
+      resident = animal.residents.find((res) => res.name === animalName);
+      if (resident) {
+        anim["name"] = resident.name;
+        anim["sex"] = resident.sex;
+        anim["age"] = resident.age;
+        anim["species"] = animal.name;
+      }
+    });
+  }
+  return anim;
 }
 
 function employeesByIds(ids) {
-  // your code here
+  if (!ids) return [];
+
+  if (typeof ids === "string") {
+    return [zooData.employees.find((emp) => emp.id === ids)];
+  } else if (Array.isArray(ids)) {
+    return zooData.employees.filter((employee) => {
+      for (let id of ids) {
+        if (id === employee.id) return true;
+      }
+    });
+  }
 }
 
 function employeeByName(employeeName) {
-  // your code here
+  let employee = {};
+  if (employeeName) {
+    const target = zooData.employees.find((employee) => {
+      if (
+        employee.firstName === employeeName ||
+        employee.lastName === employeeName
+      ) {
+        return true;
+      }
+    });
+
+    if (target) {
+      employee = target;
+    }
+  }
+  return employee;
 }
 
 function managersForEmployee(idOrName) {
